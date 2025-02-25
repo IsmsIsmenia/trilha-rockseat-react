fe98f98323fef805b8d24afadf8bc8d6ed72a398
Ismenia Cristina <snowismscris@gmail.com>
2025-02-25 11:44:11 -0300

    erro de sintaxe

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -18,7 +18,7 @@ export default async function InvitePage(props: InvitePageProps) {
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
 			<div className="flex flex-col gap-10 w-full max-x-[550px]">
-				<Image src={logo} alt="devstage" width={108.5} heigth={30} />
+				<Image src={logo} alt="devstage" width={108.5} height={30} />
 
 				<div className="space-y-2">
 					<h1 className=" text-4xl font-semibold font-heading text-gray-100 leading-none">
